bdb985b0ef47a5569d87b123cbe6eb4fddfe15ab
bivoje <bivoje49@gist.ac.kr>
2017-05-25 15:01:31 +0900

    Add empty response to server

--- server.hs
+++ server.hs
@@ -4,11 +4,19 @@ import System.IO
 htmlRes :: String
 htmlRes = "\
   \HTTP/0.1 200 OK\r\n\
+  \Server: Networking Project Team\
   \Content-Type: text/html\r\n\
   \Content-Length: 14\r\n\
   \Connection: close\r\n\
   \\r\n\
   \Hello World!\r\n\
+  \"
+
+simpleHtmlRes :: String
+simpleHtmlRes = "\
+  \HTTP/01.1 200 OK\r\n\
+  \Connection: close\r\n\
+  \\r\n\
   \"
 
 main :: IO ()
@@ -22,6 +30,6 @@ handleConnections sock = do
   (handle, host, port) <- accept sock
   req <- hGetLine handle
   putStrLn $ "Got request starting with " ++ req
-  hPutStrLn handle htmlRes
+  hPutStrLn handle simpleHtmlRes
   putStrLn $ "Sent respond to " ++ show host ++ " " ++ show port
   handleConnections sock
